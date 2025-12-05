da8a4964516ef9ea0a86acc58662afdef66f6286
amaliyahnurhaida <amaliyahnurhaida@gmail.comgit config --global user.email amaliyahnurhaida@gmail.comgit config --global user.email amaliyahnurhaida@gmail.com>
2025-12-05 12:12:00 +0700

    mengerjakan tugas

---
+++ src/main/java/id/ac/unpas/praktikumpemograman2/Modul08/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.praktikumpemograman2.Modul08;
+
+/**
+ *
+ * @author Amaliyah
+ */
+
+import id.ac.unpas.praktikumpemograman2.Modul08.model.PersegiPanjangModel;
+import id.ac.unpas.praktikumpemograman2.Modul08.view.PersegiPanjangView;
+import id.ac.unpas.praktikumpemograman2.Modul08.controller.PersegiPanjangController;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 1. Instansiasi Model
+        PersegiPanjangModel model = new PersegiPanjangModel();
+
+        // 2. Instansiasi View
+        PersegiPanjangView view = new PersegiPanjangView();
+
+        // 3. Instansiasi Controller (Hubungkan Model & View)
+        PersegiPanjangController controller = new PersegiPanjangController(model, view);
+
+        // 4. Tampilkan View
+        view.setVisible(true);
+    }
+}
