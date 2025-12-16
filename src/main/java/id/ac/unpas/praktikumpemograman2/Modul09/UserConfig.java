0afb219cf4e54f1b6a6a35e1412568364bbf478a
amaliyahhaida <amaliyahnurhaida@gmail.com>
2025-12-16 22:50:35 +0700

    mengerjakan tugas

---
+++ src/main/java/id/ac/unpas/praktikumpemograman2/Modul09/UserConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.praktikumpemograman2.Modul09;
+
+/**
+ *
+ * @author Amaliyah
+ */
+
+// Latihan 3: Class untuk serialisasi objek
+import java.io.Serializable;
+
+public class UserConfig implements Serializable {
+    private String username;
+    private int fontSize;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+}
